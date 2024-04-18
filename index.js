2eff00c9d8ed345f7ea32f3c32a65f738a426b48
LokeshV790 <lokeshvazirani29@gmail.com>
2024-04-18 10:00:11 +0530

    Registry:Server: Connect to MongoDB
    Signed-off-by: LokeshV790 <lokeshvazirani29@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
+dotenv.config();
 
-app.listen(8800, ()=>{
-    console.log("connected to backend..on port : 8800");
-})
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB.");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+
+app.listen(8800, () => {
+  console.log("Connected to backend..on port : 8800");
+  connect();
+});
